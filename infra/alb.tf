f679680f85e5032ed6b355bd0ce18164bd89f743
dvorac <dustin.vorac@gmail.com>
2024-10-19 22:19:09 -0500

    temp update for infra

--- infra/alb.tf
+++ infra/alb.tf
@@ -1,81 +1,34 @@
-module "app_alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 8.4.0"
+resource "aws_lb" "ecs_alb" {
+ name               = "ecs-alb"
+ internal           = false
+ load_balancer_type = "application"
+ security_groups    = [aws_security_group.security_group.id]
+ subnets            = [aws_subnet.public1.id, aws_subnet.public2.id]
 
-  load_balancer_type = "application"
-  security_groups    = [ module.vpc_endpoints.security_groups ]
-  subnets            = module.vpc.public_subnets
-  vpc_id             = module.vpc.vpc_id
-
-  security_group_rules = {
-    ingress_all_http = {
-      type        = "ingress"
-      from_port   = 80
-      to_port     = 80
-      protocol    = "TCP"
-      description = "Permit incoming HTTP requests from the internet"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress_all = {
-      type        = "egress"
-      from_port   = 0
-      to_port     = 0
-      protocol    = "-1"
-      description = "Permit all outgoing requests to the internet"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-
-  http_tcp_listeners = [
-    {
-      # * Setup a listener on port 80 and forward all HTTP
-      # * traffic to target_groups[0] defined below which
-      # * will eventually point to our "Hello World" app.
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
-
-  target_groups = [
-    {
-      backend_port     = local.app.container_port
-      backend_protocol = "HTTP"
-      target_type      = "ip"
-    }
-  ]
+ tags = {
+   Name = "ecs-alb"
+ }
 }
 
-output "alb_url" {
-  value = "http://${module.app_alb.lb_dns_name}"
+resource "aws_lb_listener" "ecs_alb_listener" {
+ load_balancer_arn = aws_lb.ecs_alb.arn
+ port              = 80
+ protocol          = "HTTP"
+
+ default_action {
+   type             = "forward"
+   target_group_arn = aws_lb_target_group.ecs_tg.arn
+ }
 }
 
-# resource "aws_lb" "api_lb" {
-#   name = "ukol-api-lb"
-#   subnets = aws_subnet.public[*].id
-#   security_groups = [ aws_security_group.lb.id ]
-# }
+resource "aws_lb_target_group" "ecs_tg" {
+ name        = "ecs-target-group"
+ port        = 80
+ protocol    = "HTTP"
+ target_type = "ip"
+ vpc_id      = aws_vpc.main.id
 
-# resource "aws_lb_target_group" "lb" {
-#   name = "ukol-lb-group"
-#   port = 3333
-#   protocol = "HTTP"
-#   vpc_id = aws_vpc.vpc.id
-#   target_type = "ip"
-#   health_check {
-#     path = "/info"
-#     matcher = "200,301,302"
-#   }
-# }
-
-# resource "aws_lb_listener" "lb" {
-#   load_balancer_arn = aws_lb.lb.id
-#   port = "443"
-#   protocol = "HTTPS"
-#   certificate_arn = data.aws_acm_certificate.cert.arn
-
-#   default_action {
-#     target_group_arn = aws_lb_target_group.lb.id
-#     type = "forward"
-#   }
-# }
+ health_check {
+   path = "/"
+ }
+}
--- infra/api/alb.tf
+++ infra/api/alb.tf
@@ -1,77 +1,77 @@
-# module "app_alb" {
-#  source  = "terraform-aws-modules/alb/aws"
-#  version = "~> 8.4.0"
+module "app_alb" {
+ source  = "terraform-aws-modules/alb/aws"
+ version = "~> 8.4.0"
 
-#  load_balancer_type = "application"
-#  security_groups = [module.vpc.default_security_group_id]
-#  subnets = module.vpc.public_subnets
-#  vpc_id = module.vpc.vpc_id
+ load_balancer_type = "application"
+ security_groups = [module.vpc.default_security_group_id]
+ subnets = module.vpc.public_subnets
+ vpc_id = module.vpc.vpc_id
 
-#  security_group_rules = {
-#   ingress_all_http = {
-#    type        = "ingress"
-#    from_port   = 80
-#    to_port     = 80
-#    protocol    = "TCP"
-#    description = "Permit incoming HTTP requests from the internet"
-#    cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   egress_all = {
-#    type        = "egress"
-#    from_port   = 0
-#    to_port     = 0
-#    protocol    = "-1"
-#    description = "Permit all outgoing requests to the internet"
-#    cidr_blocks = ["0.0.0.0/0"]
-#   }
-#  }
+ security_group_rules = {
+  ingress_all_http = {
+   type        = "ingress"
+   from_port   = 80
+   to_port     = 80
+   protocol    = "TCP"
+   description = "Permit incoming HTTP requests from the internet"
+   cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress_all = {
+   type        = "egress"
+   from_port   = 0
+   to_port     = 0
+   protocol    = "-1"
+   description = "Permit all outgoing requests to the internet"
+   cidr_blocks = ["0.0.0.0/0"]
+  }
+ }
 
-#  http_tcp_listeners = [
-#   {
-#    # * Setup a listener on port 80 and forward all HTTP
-#    # * traffic to target_groups[0] defined below which
-#    # * will eventually point to our "Hello World" app.
-#    port               = 80
-#    protocol           = "HTTP"
-#    target_group_index = 0
-#   }
-#  ]
+ http_tcp_listeners = [
+  {
+   # * Setup a listener on port 80 and forward all HTTP
+   # * traffic to target_groups[0] defined below which
+   # * will eventually point to our "Hello World" app.
+   port               = 80
+   protocol           = "HTTP"
+   target_group_index = 0
+  }
+ ]
 
-#  target_groups = [
-#   {
-#    backend_port         = local.container_port
-#    backend_protocol     = "HTTP"
-#    target_type          = "ip"
-#   }
-#  ]
+ target_groups = [
+  {
+   backend_port         = local.container_port
+   backend_protocol     = "HTTP"
+   target_type          = "ip"
+  }
+ ]
+}
+
+# resource "aws_lb" "api_lb" {
+#   name = "ukol-api-lb"
+#   subnets = aws_subnet.public[*].id
+#   security_groups = [ aws_security_group.lb.id ]
 # }
 
-# # resource "aws_lb" "api_lb" {
-# #   name = "ukol-api-lb"
-# #   subnets = aws_subnet.public[*].id
-# #   security_groups = [ aws_security_group.lb.id ]
-# # }
+# resource "aws_lb_target_group" "lb" {
+#   name = "ukol-lb-group"
+#   port = 3333
+#   protocol = "HTTP"
+#   vpc_id = aws_vpc.vpc.id
+#   target_type = "ip"
+#   health_check {
+#     path = "/info"
+#     matcher = "200,301,302"
+#   }
+# }
 
-# # resource "aws_lb_target_group" "lb" {
-# #   name = "ukol-lb-group"
-# #   port = 3333
-# #   protocol = "HTTP"
-# #   vpc_id = aws_vpc.vpc.id
-# #   target_type = "ip"
-# #   health_check {
-# #     path = "/info"
-# #     matcher = "200,301,302"
-# #   }
-# # }
+# resource "aws_lb_listener" "lb" {
+#   load_balancer_arn = aws_lb.lb.id
+#   port = "443"
+#   protocol = "HTTPS"
+#   certificate_arn = data.aws_acm_certificate.cert.arn
 
-# # resource "aws_lb_listener" "lb" {
-# #   load_balancer_arn = aws_lb.lb.id
-# #   port = "443"
-# #   protocol = "HTTPS"
-# #   certificate_arn = data.aws_acm_certificate.cert.arn
-
-# #   default_action {
-# #     target_group_arn = aws_lb_target_group.lb.id
-# #     type = "forward"
-# #   }
-# # }
+#   default_action {
+#     target_group_arn = aws_lb_target_group.lb.id
+#     type = "forward"
+#   }
+# }
